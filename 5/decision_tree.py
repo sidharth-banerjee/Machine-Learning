e28974681e7d1023356cfc98be8290d2fbb26d2f
sidharth-banerjee <sidharth.rsb@gmail.com>
2019-10-26 21:13:16 -0500

    DTL top level

--- 5/decision_tree.py
+++ 5/decision_tree.py
@@ -9,6 +9,8 @@ import random
 import numpy as np
 import pandas as pd
 
+from node import Node
+
 # command line arguments
 training_file = str(sys.argv[1])
 test_file = str(sys.argv[2])
@@ -27,5 +29,25 @@ def returnData(training_file):
     df.columns = col_header
     return df
 
+def class_distribution(df):
+    class_count = [0] * (df['Class'].max())
+    for i in range (0, len(df)):
+        class_count[int(df.iloc[i]['Class'])-1] += 1
+    class_probability = np.array(class_count)/1000
+    return class_probability
+
+def DTL_TopLevel(df, pruning_thr):
+    attributes = len(df.columns)-1
+    default = np.argmax(class_distribution(df)) + df['Class'].min()
+    return (np.array(df), attributes, default, pruning_thr)
+
+#def DTL(examples, attributes, default, pruning_thr):
+#    if np.size(examples) == 0:
+#        return default
+
+#    elif(sameClass(examples)):
+#        return examples[0][-1]
+
 df_train = returnData(training_file)
-print(df_train.head())
+
+arr, att, default, pru = DTL_TopLevel(df_train, pruning_thr)
